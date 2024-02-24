bd45cd60e37d87edc8a42b1ffe537800be8b326c
Avik <shaancse08@gmail.com>
2024-02-24 10:58:58 +0530

    List report generated with Annotations

---
+++ app/galacticlistreport/webapp/test/integration/FirstJourney.js
@@ -0,0 +1,36 @@
+sap.ui.define([
+    "sap/ui/test/opaQunit"
+], function (opaTest) {
+    "use strict";
+
+    var Journey = {
+        run: function() {
+            QUnit.module("First journey");
+
+            opaTest("Start application", function (Given, When, Then) {
+                Given.iStartMyApp();
+
+                Then.onTheGalacticSpacefarerList.iSeeThisPage();
+
+            });
+
+
+            opaTest("Navigate to ObjectPage", function (Given, When, Then) {
+                // Note: this test will fail if the ListReport page doesn't show any data
+                When.onTheGalacticSpacefarerList.onFilterBar().iExecuteSearch();
+                Then.onTheGalacticSpacefarerList.onTable().iCheckRows();
+
+                When.onTheGalacticSpacefarerList.onTable().iPressRow(0);
+                Then.onTheGalacticSpacefarerObjectPage.iSeeThisPage();
+
+            });
+
+            opaTest("Teardown", function (Given, When, Then) { 
+                // Cleanup
+                Given.iTearDownMyApp();
+            });
+        }
+    }
+
+    return Journey;
+});
--- srv/galacticSpacefarer.cds
+++ srv/galacticSpacefarer.cds
@@ -3,13 +3,12 @@ using galactic.spacefarer as spacefarer from '../db/data-model';
 @path: '/GalacticSRV'
 service GalacticService {
     entity GalacticSpacefarer as projection on spacefarer.GalacticSpacefarer;
+    entity Department         as projection on spacefarer.Department;
 
     @readonly
-    entity Department as projection on spacefarer.Department;
-
-    @readonly
-    entity Position   as projection on spacefarer.Position;
+    entity Position           as projection on spacefarer.Position;
 }
+
 
 
 
